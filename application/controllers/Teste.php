13f58de0b93066cfdefe09bdd9550487b6f8a0aa
Wesley Nunes <wesleysilva.ti@gmail.com>
2019-03-22 18:52:47 -0300

    first commit

---
+++ application/controllers/Teste.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Teste extends CI_Controller
+{
+
+    public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->library(['ion_auth', 'form_validation']);
+		$this->load->helper(['url', 'language']);
+
+		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+
+		$this->lang->load('auth');
+	}
+
+
+    public function index()
+    {
+
+        if (!$this->ion_auth->logged_in())
+		{
+			// redirect them to the login page
+			redirect('auth/login', 'refresh');
+		}
+		else if (!$this->ion_auth->is_admin()) // remove this elseif if you want to enable this for non-admins
+		{
+			// redirect them to the home page because they must be an administrator to view this
+			show_error('You must be an administrator to view this page.');
+		}
+		else
+		{
+            $this->load->view("teste_view");
+        }
+    }
+}
